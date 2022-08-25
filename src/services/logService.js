4a9e5799a68cf5f8c4bdf380de889f4e50bd01eb
LegendaryCodeking <legendarycodeking0908@gmail.com>
2022-08-25 12:14:46 +0300

    House keeping

--- src/services/logService.js
+++ src/services/logService.js
@@ -5,7 +5,7 @@ const init = () => {
     Sentry.init({
         dsn: "https://ef3f7aab52904fc8aaf619a7ef41dfc7@o1361282.ingest.sentry.io/6649872",
         integrations: [new BrowserTracing()],
-        release: "Caspre-io@" + process.env.npm_package_version,
+        release: process.env.REACT_APP_NAME + "@" + process.env.REACT_APP_VERSION,
 
         // Set tracesSampleRate to 1.0 to capture 100%
         // of transactions for performance monitoring.
